a1eb4cd99e9d5df849d254ee445d2dc0a281339f
Javier Combita Tellez <javiercombita2014@gmail.com>
2024-01-10 11:47:01 -0500

    avances en el codigo

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,5 @@
 import '../scss/style.scss'
+import * as bootstrap from 'bootstrap'
 
 //Selectores
 const piso=document.getElementById("select_floor")
@@ -6,6 +7,7 @@ const canecas =document.querySelectorAll(".bowl")
 
 canecas.forEach(element => {
     element.addEventListener("click",function (e) {
-        console.log("le diste click a una caneca");
+        document.getElementById("btnOpenModal").click()
+        console.log("le diste click a una caneca",element.getAttribute("type-bowl"));
     })
 });
